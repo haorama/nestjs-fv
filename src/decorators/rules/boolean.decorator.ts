e415cc0607317f46c11ef51edef0ef73723d38a2
mlazuardy <lazuardymichael@gmail.com>
2023-03-18 21:02:35 +0700

    remove @see

--- src/decorators/rules/boolean.decorator.ts
+++ src/decorators/rules/boolean.decorator.ts
@@ -4,7 +4,6 @@ import { addRule } from "../../metadata/storage.metadata";
 /**
  * Decorator for adding a boolean rule to a property
  * by default, it will convert the value to a boolean
- * @see https://github.com/icebob/fastest-validator#boolean
  */
 export function IsBoolean(options?: Partial<RuleBoolean>): PropertyDecorator {
   return (target: any, propName: string) => {
